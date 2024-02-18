88885613b3e8429f6b57a83e2ae21b7d9531629e
Saneef Ansari <hello@saneef.com>
2024-02-18 21:15:52 +0530

    fix(fonts): left double angle quotation was missing

--- src/fonts/subset.fish
+++ src/fonts/subset.fish
@@ -1,10 +1,9 @@
 #!/usr/bin/env fish
 
 # Use https://www.zachleat.com/unicode-range-interchange/ to merge all ranges
-# Base: U+20-7E,U+A9,U+B0,U+BB,U+D7,U+2013,U+2014,U+2018-2019,U+201C-201D,U+2026
-# Arrows: U+2190-2199
+# Base: U+20-7E,U+A9,U+AB,U+B0,U+BB,U+D7,U+2013,U+2014,U+2018,U+2019,U+201C,U+201D,U+2026
 
-set ranges "U+20-7E,U+A9,U+B0,U+BB,U+D7,U+2013,U+2014,U+2018,U+2019,U+201C,U+201D,U+2026,U+2190-2199"
+set ranges "U+20-7E,U+A9,U+AB,U+B0,U+BB,U+D7,U+2013,U+2014,U+2018,U+2019,U+201C,U+201D,U+2026"
 
 set files "PlaypenSans-Regular.ttf" "PlaypenSans-Bold.ttf"
 
